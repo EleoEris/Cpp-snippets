cdbe8f01bfe6a850275ed783ff8d0ac98eb0058f
EleoEris <79694371+EleoEris@users.noreply.github.com>
2022-07-23 22:44:54 +0200

    Better comment and formatting

--- iniParser.cpp
+++ iniParser.cpp
@@ -7,7 +7,7 @@
 std::map<std::string, std::string> parseIni(std::string path) {
 	std::ifstream ifs = std::ifstream(path);
 	std::string line = std::string();
-	std::map<std::string, std::string>	retval = std::map<std::string, std::string>();
+	std::map<std::string, std::string> retval = std::map<std::string, std::string>();
 	// the pattern below should be the most idiot-proof pattern ever
 	std::regex match_pattern = std::regex("^([a-zA-Z0-9_\\-]*)(?:\\s*(?:\\\\t)*\\s*=\\s*(?:\\\\t)*\\s*)([a-zA-Z0-9_\\-]*)", std::regex_constants::optimize);
 	std::smatch match = std::smatch();
@@ -16,7 +16,7 @@ std::map<std::string, std::string> parseIni(std::string path) {
 		while (ifs.good()) {
 			std::getline(ifs, line);
 			std::regex_search(line, match, match_pattern);
-			if (match.size() == 3) // match[0] is the whole line if there is a match
+			if (match.size() == 3) // match[0] is the whole line
 				retval.insert_or_assign(match[1], match[2]);
 		}
 	} else {
