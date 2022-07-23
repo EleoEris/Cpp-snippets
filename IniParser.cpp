654a9d98ed4ea92e5668ad21b589a03e4c300fb1
EleoEris <79694371+EleoEris@users.noreply.github.com>
2022-07-23 19:37:44 +0200

    Deleted debug features

--- IniParser.cpp
+++ IniParser.cpp
@@ -8,6 +8,7 @@ std::map<std::string, std::string> parseIni(std::string path) {
 	std::ifstream ifs = std::ifstream(path);
 	std::string line = std::string();
 	std::map<std::string, std::string>	retval = std::map<std::string, std::string>();
+	// the pattern below should be the most idiot-proof pattern ever
 	std::regex match_pattern = std::regex("^([a-zA-Z0-9_\\-]*)(?:\\s*(?:\\\\t)*\\s*=\\s*(?:\\\\t)*\\s*)([a-zA-Z0-9_\\-]*)", std::regex_constants::optimize);
 	std::smatch match = std::smatch();
 
@@ -17,17 +18,10 @@ std::map<std::string, std::string> parseIni(std::string path) {
 			std::regex_search(line, match, match_pattern);
 			if (match.size() < 3)
 				continue;
-
-			std::cout << "inside: " << match[1].str() << " - " << match[2].str() << "\n";
 			retval.insert_or_assign(match[1], match[2]);
 		}
-
 	} else {
-		printf("Could not load the file!" "ifs.is_open() == false");
-	}
-
-	for (std::pair<std::string, std::string> x : retval) {
-		std::cout << x.first << ": " << x.second << "\n";
+		std::cout << "\x1B[31mCould not load the file!\033[0m ifs.is_open() == false\n";
 	}
 
 	return retval;
