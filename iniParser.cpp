3e66aa64e3df4dcff2b984c0ea6b4f384573ef23
EleoEris <79694371+EleoEris@users.noreply.github.com>
2022-07-27 15:04:06 +0200

    changed type to unordered map

--- iniParser.cpp
+++ iniParser.cpp
@@ -1,33 +1,37 @@
-#include <map>
+#include <unordered_map>
 #include <string>
 #include <fstream>
 #include <regex>
-#include <iostream>
 
-std::map<std::string, std::string> mapFromIni(std::string path, int maxSettings = 255) {
+std::unordered_map<std::string, std::string> mapFromIni(std::string path, int maxSettings, std::string errorKey) {
 	std::ifstream ifs = std::ifstream(path);
-	// ini is potentially user input, so I want to avoid mods that would try to crash the game by loading too many strings
-	// 255 seems like a decent number
 	int counter = 0; 
 
 	std::string line = std::string();
-	std::map<std::string, std::string> retval = std::map<std::string, std::string>();
+	std::unordered_map<std::string, std::string> retval = std::unordered_map<std::string, std::string>();
 	
 	std::regex match_pattern = std::regex("^([a-zA-Z0-9_\\-]*)(?:\\s*(?:\\\\t)*\\s*=\\s*(?:\\\\t)*\\s*)([a-zA-Z0-9_\\-]*)", std::regex_constants::optimize);
 	std::smatch match = std::smatch();
 
+	retval.insert_or_assign(errorKey, "");
+
 	if (ifs.is_open()) {
-		while (ifs.good() && counter < maxSettings) {
+		while (ifs.good()) {
 			std::getline(ifs, line);
 			std::regex_search(line, match, match_pattern);
-			if (match.size() == 3) // match[0] is the whole line
+			if (match.size() == 3) { // match[0] is the whole line
 				counter++;
 				retval.insert_or_assign(match[1], match[2]);
+			}
+			if (counter > maxSettings) {
+				retval.insert_or_assign(errorKey, "Too many lines in settings!");
+				break;
+			}
 		}
-		ifs.close();
 	} else {
-		printf_s("\x1B[31mCould not load the file!\033[0m ifs.is_open() == false\n");
+		retval.insert_or_assign(errorKey, "Could not load the file! ifs.is_open() == false");
 	}
+	ifs.close();
 
 	return retval;
 }
